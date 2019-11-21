5c699755b314645df826802c0891de0bab7cbaa9
Jeremy <jeremy@radialogica.com>
2019-11-21 11:57:08 -0600

    Added frontend for interacting with backend data

---
+++ app/controllers/dummies.js
@@ -0,0 +1,27 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+
+export default Controller.extend({
+  store: service(),
+  name: null,
+  number: null,
+  actions: {
+    destroyDummy(id) {
+      const store = this.get('store');
+      store.findRecord('dummy', id).then(dummy => { dummy.destroyRecord(); });
+    },
+    updateDummy(id, name, number) {
+      const store = this.get('store');
+      store.findRecord('dummy', id).then(dummy => {
+        dummy.set('name', name);
+        dummy.set('number', number);
+        dummy.save();
+      });
+    },
+    createDummy(name, number) {
+      const store = this.get('store');
+      const dummy = store.createRecord('dummy', { name: name, number: number });
+      dummy.save();
+    }
+  }
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('dummies', { path: '/' });
 });
 
 export default Router;
